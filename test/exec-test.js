6ca9297437696523ba12d895fd096ef1a959c099
indexzero <charlie.robbins@gmail.com>
2011-08-09 21:09:09 -0400

    [api test] Small updates from nexpect from vows-based unit testing

--- test/exec-test.js
+++ test/exec-test.js
@@ -1,24 +1,44 @@
-/* not implemented 
-
-var sys = require('sys'),
-    events = require('events'),
-    assert = require('assert'),
-    eyes = require('eyes'),
+/*
+ * nexpect-test.js: Tests for the `nexpect` module.
+ *
+ * (C) 2011, Elijah Insua, Marak Squires, Charlie Robbins.
+ *
+ */
+ 
+var assert = require('assert'),
     vows = require('vows'),
-    exec = require('child_process').exec,
     spawn = require('child_process').spawn,
-    nspawn = require('../lib/nexpect').nspawn;
+    nexpect = require('../lib/nexpect');
 
-vows.describe('noc/terminal-exec').addBatch({
-  "When executing commands on the terminal": {
-    "and you run ls with DIRNAME": {
-      topic: nspawn.spawn('ls test -a'),
-      "you should see the current path": function(result){
-        eyes.inspect(result);
-        assert.equal( result, 'exec-test.js');
-      }
+function assertSpawn (expect) {
+  return {
+    topic: function () {
+      expect.run(this.callback)
+    },
+    "should respond with no error": function (err) {
+      assert.isTrue(!err);
+    }
+  }
+}
+
+vows.describe('nexpect').addBatch({
+  "When using the nexpect module": {
+    "spawning": {
+      "`echo hello`": assertSpawn(
+        nexpect.spawn("echo", ["hello"])
+               .expect("hello")
+      ),
+      "`ls -al /tmp/undefined`": assertSpawn(
+        nexpect.spawn("ls -la /tmp/undefined")
+               .expect("No such file or directory")
+      ),
+      "and using the sendline() method": assertSpawn(
+        nexpect.spawn("node")
+              .expect(">")
+              .sendline("console.log('testing')")
+              .expect("testing")
+              .sendline("process.exit()")
+      )
     }
   }
 }).export(module);
-
-*/
